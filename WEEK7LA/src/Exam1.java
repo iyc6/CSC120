f2d19dd49e07a865fdb071b60002ab16350c3b3e
Ivy <iyc6@miami.edu>
2024-10-21 11:01:00 -0400

    Good

---
+++ WEEK7LA/src/Exam1.java
@@ -0,0 +1,26 @@
+public class Exam1 {
+    //==============================================================
+    public class PowerfulArray {
+//--------------------------------------------------------------
+    private static final int NUMBER_OF_ELEMENTS = 6;
+    private static final int POWER_EXPONENT = 3;
+//-------------------------------------------------------------
+    public static void main(String[] args) {
+
+//----
+
+            double [] array = new double[NUMBER_OF_ELEMENTS];
+            int index;
+
+            for (index = 0; index < NUMBER_OF_ELEMENTS; index++) {
+                array[index] = Math.pow(index, POWER_EXPONENT);
+            }
+
+
+
+
+    }
+//--------------------------------------------------------------
+    }
+//==============================================================
+}
